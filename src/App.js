e8cb3586d8a8ac145bab6fdb7457aaaf115434b6
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-16 18:13:54 -0500

    Initializing useState in a component  and using onChange methon in the self closing input tag

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 //---------------Imports
 import responses from './models/responses';
 import Button from './Components/Button';
+import { useState, useEffect } from 'react';
 
 
-// 1.Initializing the useState
+
+
 
 
 function App() {
-  // 1.Initializing the useState
+  // 1.Initializing the useState (i.e. Setting Initail State in a Component using useState)
+   // Create a fxn for the input tag with userInput as the variable containing the state and setUserInput as the fxn that will update the state ,and usestate as the initial state
+  const [userInput, setUserInput] = useState('')
+     //2. Create a fxn for the randomizing the responses from the models file with randomIndax as the variable and setRandomIndex as the fxn and usestate as the initial state
+     const [randomIndax, setRandomIndex] = useState(``)
+// 3. useEffect hook - 2 parts; a callback function where logic or code is written and a dependency array that runs the logic whenever there is a change 
+     useEffect(() => {
+        console.log( 'Your question')
+     },
+     [userInput]);
+
+  
+
 
 
   return (
-    // Creating the structure of the page with a parent dic with className, App and a child div
+    // Creating the structure of the page with a parent div with className, App and a child div having a button
     <div className="App">
        <h1> Welcome to Magic 8 Ball Chamber </h1>
        <p className='info' > Ask Whatever You Like and Press the Try Again Button to see your Furture..  </p>
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,23 @@
 // Open the jsx boilerplate using rfce
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
-function Button() {
+function Button(props) {
+//Initializing the useState (i.e. Setting Initail State in a Component using useState)
+    const [userInput, setUserInput] = useState('')
+
+    //Input with onChange method // create a method ( also know as a call back function) called handleQuestionChange with parameter event
+      const handleQuestionChange = (event) => {
+        //To prevent  the page from reloading upon clicking the Try Again button, use the event.preventDefault method
+        event.preventDefault();
+        // To access the string of responses,use event.target.value and //Update the userInput variable by using setUserInput
+        console.log(event.target.value);
+        setUserInput(event.target.value);
+      }
+    //Add an attribute called value to the self closing input tag
+    <input type="text" className="question" value={userInput}/>
+ 
+
   return (
     <div>
         <button> Try Again </button>
